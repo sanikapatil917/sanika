a5cd4b82a5a085dbe5db75b15cb58a9676538ca9
sanikapatil917 <sanikapatil2934@gmail.com>
2024-08-20 17:09:10 +0530

    Update landing-page.js

--- landing-page.js
+++ landing-page.js
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
 
-    <title>Form Validation Example</title>
+    <title>Form Validation</title>
     <style>
         
-    <h1>Form Validation Example</h1>
+    <h1>Form Validation </h1>
     <form id="myForm">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name">
